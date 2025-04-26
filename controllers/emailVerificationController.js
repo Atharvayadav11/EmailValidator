46ab9f420486bb78f649582ff352af58d45f7d46
Atharva Yadav <atharvayadav11@gmail.com>
2025-04-26 18:29:12 +0530

    fix

--- controllers/emailVerificationController.js
+++ controllers/emailVerificationController.js
@@ -107,8 +107,7 @@ async function savePersonData(personData, company, verifiedEmail, allResults) {
  * Find work email based on personal details
  */
 async function findWorkEmail(req, res) {
-   // Inside findWorkEmail function
-const requestId = `req_${Date.now()}_${Math.random().toString(36).substring(2, 10)}`;
+    const requestId = Logger.generateRequestId();
     const startTime = Date.now();
     
   try {
